a27f959969401a341d5babd086ca64f3c598ded3
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-05 21:18:56 -0700

    audio: Use normal tinycompress
    Change-Id: I09cf545684517e1fec9d5f6113587f2257ce718e

--- hal/Android.mk
+++ hal/Android.mk
@@ -239,7 +239,7 @@ LOCAL_SHARED_LIBRARIES := \
         liblog \
         libcutils \
         libtinyalsa \
-        libtinycompress_vendor \
+        libtinycompress \
         libaudioroute \
         libdl \
         libaudioutils \
